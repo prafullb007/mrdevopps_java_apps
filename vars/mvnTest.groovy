7b5884a31de0492002780e930f3d3f5250b63814
prafullb007 <127543775+prafullb007@users.noreply.github.com>
2024-05-22 17:33:39 +0530

    Update mvnTest.groovy

--- vars/mvnTest.groovy
+++ vars/mvnTest.groovy
@@ -1,3 +1,9 @@
 def call(){
+    sh 'mvn clean'
+    sh 'mvn compile'
+    sh 'mvn install'
+    sh 'mvn clean install -e'
+    sh 'mvn clean install -X'
+    sh 'mvn validate'
     sh 'mvn test'
 }
